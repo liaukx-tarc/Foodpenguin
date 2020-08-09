0587c25d69df8200a66a856d09fe21f4cd5dad6e
liaukx-tarc <liaukx@student.tarc.edu.my>
2020-08-09 18:43:05 +0800

    Foodpenguin add login screen and done Splash Screen

---
+++ app/src/main/java/com/xhpp/foodpenguin/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.xhpp.foodpenguin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this , MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
