4f065581308ce8eb47907a62336ff0e1b4ee9477
liaukx-tarc <liaukx@student.tarc.edu.my>
2020-08-13 05:16:18 +0800

    Done "Register Activity" layout added "ic_penguin_logo" (have FOODPENGUIN text)

--- app/src/main/java/com/xhpp/foodpenguin/ui/register/RegisterActivity.java
+++ app/src/main/java/com/xhpp/foodpenguin/ui/register/RegisterActivity.java
@@ -2,9 +2,12 @@ package com.xhpp.foodpenguin.ui.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.xhpp.foodpenguin.R;
+import com.xhpp.foodpenguin.ui.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -12,5 +15,12 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void signIn(View view)
+    {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
