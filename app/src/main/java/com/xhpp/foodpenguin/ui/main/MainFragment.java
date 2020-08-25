68c6d676fae01f4f589fc80fb729da017dcafe81
Zheng240901 <tongzh-wm19@student.tarc.edu.my>
2020-08-25 14:38:16 +0800

    Update MainFragment.java

--- app/src/main/java/com/xhpp/foodpenguin/ui/main/MainFragment.java
+++ app/src/main/java/com/xhpp/foodpenguin/ui/main/MainFragment.java
@@ -7,14 +7,11 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+
 
 import com.xhpp.foodpenguin.R;
 
